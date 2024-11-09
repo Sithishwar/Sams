f27d27d123760db53925acafa933f463b13eb372
Prakash <saiprakash6301@gmail.com>
2024-11-09 16:01:16 +0530

    Updated the login page

--- sams/src/Login.jsx
+++ sams/src/Login.jsx
@@ -6,15 +6,30 @@ function Login()
     return (
         <>
         <div id="header">
-           <h1>SAMS</h1>  
+           <h1>STUDENT ATTENDENCE MANAGEMENT SYSTEM</h1>  
         </div>
         <div id ='mainbloc'>
         <div>
-        <label htmlfor="fname">User Name:</label>
-        <input id="fname" placeholder='enter name' required ></input>
-        <br/><label htmlfor="pass" >Password:</label>
-        <input id="pass" placeholder='enter password' required></input>
-        <br/></div>
+        <table>
+            
+            <tr>
+                <td>
+                    <label htmlfor="fname">User Name</label>
+                </td>
+                <td>:</td>
+                <td>
+                    <input id="fname" placeholder='enter name' required ></input>
+                </td>
+            </tr>
+            <tr>
+                <td><label htmlfor="pass" >Password</label></td>
+                <td>:</td>
+                <td>
+                    <input id="pass" placeholder='enter password' required></input>
+                </td>
+            </tr>
+        </table>
+        </div>
         <a href="">forget password?</a>
         <button>Login</button>
         <button>Sign Up</button>
